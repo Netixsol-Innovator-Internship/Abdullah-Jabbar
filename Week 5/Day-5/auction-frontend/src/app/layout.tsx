cd60a0513086fdbc265ddbcd49edd79743f8a3d3
Leo-Planer <abdullah_jabbar_intern@netixsol.com>
2025-09-03 10:28:35 +0500

    applied max-w

--- Week 5/Day-5/auction-frontend/src/app/layout.tsx
+++ Week 5/Day-5/auction-frontend/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="max-w-360 mx-auto">
         <div className="flex flex-col">
           <Header />
           <Navigation />
